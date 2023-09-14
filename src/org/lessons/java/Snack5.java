9eb68a7c07d05284b0af1c37654010ae0de34eea
gabrielepetralia <gabri.petralia03@gmail.com>
2023-09-14 14:12:06 +0200

    fix snack 5

--- src/org/lessons/java/Snack5.java
+++ src/org/lessons/java/Snack5.java
@@ -6,26 +6,37 @@ public class Snack5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Inserisci una stringa: ");
-		String str = sc.nextLine();
+		String str = null;
 		
-		int digits = 0;
-        int letters = 0;
-        int symbols = 0;
+		do {
+			System.out.print("Inserisci una stringa: ");
+			str = sc.nextLine();
+			
+			if(!str.equals("0")) {
+				int digits = 0;
+		        int letters = 0;
+		        int symbols = 0;
 
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (Character.isDigit(ch)) {
-                digits++;
-            } else if (Character.isLetter(ch)) {
-                letters++;
-            } else {
-                symbols++;
-            }
-        }
-        
-        System.out.println("\nNumeri: " + digits);
-        System.out.println("Lettere: " + letters);
-        System.out.println("Simboli: " + symbols);
+		        for (int i = 0; i < str.length(); i++) {
+		            char ch = str.charAt(i);
+		            if (Character.isDigit(ch)) {
+		                digits++;
+		            } else if (Character.isLetter(ch)) {
+		                letters++;
+		            } else {
+		                symbols++;
+		            }
+		        }
+		        
+		        System.out.println("Numeri: " + digits);
+		        System.out.println("Lettere: " + letters);
+		        System.out.println("Simboli: " + symbols +"\n");
+			} else {
+				System.out.println("Arrivederci!");
+			}
+			
+		}while(!str.equals("0"));
+		
+		sc.close();
 	}
 }
